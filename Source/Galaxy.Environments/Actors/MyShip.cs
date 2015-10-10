8a832cbf12dfb42d61e9395f1ded5b136bfd5a1b
Name <you@example.com>
2015-10-10 10:11:57 +0700

    Izmenenie

---
+++ Source/Galaxy.Environments/Actors/MyShip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Environment;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+
+
+namespace Galaxy.Environments.Actors
+{
+    class MyShip: Ship
+    {
+    
+        public MyShip(ILevelInfo info)
+            : base(info)
+        {
+            Width = 40;
+            Height = 35;
+            ActorType = ActorType.Enemy;
+        }
+
+        public override void Load()
+        {
+            base.Load();
+            Load(@"Assets\myship.png");
+            
+        }
+
+        public override void Update()
+        {
+            InitTimer();
+            EnemyBullet enbBullet = new EnemyBullet(Info);
+        }
+        
+
+    }
+}
